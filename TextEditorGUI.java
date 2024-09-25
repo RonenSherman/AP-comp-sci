67a2c071e48eeebe2b30df2ef63c3a784fa8b3f7
RonenSherman <roshersnowy@gmail.com>
2024-09-25 08:54:45 -0700

    Text Editor for java

--- TextEditorGUI.java
+++ TextEditorGUI.java
@@ -6,12 +6,18 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+import static java.nio.file.Files.readString;
 
 
 public class TextEditorGUI { //       Ronen Sherman File text editor
 
-   static ArrayList<String> FileNames = new ArrayList<String>();
-    public static void main(String[] args) {
+    static ArrayList<String> FileNames = new ArrayList<String>();
+    public static void main(String[] args)throws FileNotFoundException {
         JFrame frame = new JFrame("Text Editor");
         frame.setForeground(Color.CYAN);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,14 +30,14 @@ public class TextEditorGUI { //       Ronen Sherman File text editor
         JButton button1 = new JButton();
         button1.setText("Create new File");
         button1.setPreferredSize(new Dimension(300,100));
-         button1.addActionListener(new FileCreatePressed());
+        button1.addActionListener(new FileCreatePressed());
         frame.add(button1);
         JButton button2 = new JButton();
         button2.setText("Edit existing File");
         button2.setPreferredSize(new Dimension(300,100));
         button2.addActionListener(new EditFilePressed());
         frame.add(button2);
-       // frame.pack();
+        // frame.pack();
         frame.setVisible(true);
     }
     public static class FileCreatePressed implements ActionListener {
@@ -40,9 +46,9 @@ public class TextEditorGUI { //       Ronen Sherman File text editor
             String Filename = JOptionPane.showInputDialog(null, "How would you like to name this file?");
             try {
                 File myObj = new File(Filename + ".txt");
-                    FileNames.add(Filename);
-               if (myObj.createNewFile()) {
-                   JOptionPane.showMessageDialog(null,"File created: " + myObj.getName());
+                FileNames.add(Filename);
+                if (myObj.createNewFile()) {
+                    JOptionPane.showMessageDialog(null,"File created: " + myObj.getName());
                 } else {
                     JOptionPane.showMessageDialog(null, "File already exists.");
 
@@ -55,28 +61,62 @@ public class TextEditorGUI { //       Ronen Sherman File text editor
     }
     public static class EditFilePressed implements ActionListener { // file stuff
         @Override
-        public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e)  {
+
+
+
             String input = JOptionPane.showInputDialog(null, "Which file would you like to open?");
-            JFrame frame = new JFrame("File input ");
-            String filenames = input + ".txt";
-            Path fileName = Path.of("/Users/shermanr/IdeaProjects/AP-comp-sci/" + filenames);
-           JTextField field = new JTextField();
-            frame.add(field);
-            frame.setVisible(true);
 
-            String text = field.getText();
-         //field.write(new Writer(FileNames));
-
-
+            Path filePath = Path.of("/Users/shermanr/IdeaProjects/AP-comp-sci/" + input + ".txt");
+            String content = null;
             try {
-                Files.writeString(fileName, text);
-                String fileContent = Files.readString(fileName);
+                content = Files.readString(filePath);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            JFrame frame = new JFrame();
+            JTextField field = new JTextField();
+            frame.add(field);
+            frame.setPreferredSize(new Dimension(500,500));
+            frame.pack();
+            field.addKeyListener(EnterKeyPressed);
+            frame.setVisible(true);
+            field.setText(content);
+          /*  try {
+                Files.writeString(filePath, text);
+                String fileContent = readString(filePath);
                 System.out.println(fileContent);
             } catch (IOException f) {
 
-                System.err.println("An error occurred: " + f.getMessage());
+            System.err.println("An error occurred: " + f.getMessage());
+        }*/
+
+        }
+    }
+
+
+    public static class EnterKeyPressed implements KeyListener
+    {
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+
+            if(keyCode == KeyEvent.VK_ESCAPE)
+            {
+              JOptionPane.showConfirmDialog(null,"would you like tp save these  chanes?");
             }
 
-                }
-            }
+        }
     }
+}
