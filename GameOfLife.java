a0f42b0f2dfcaeadaf65d060759a691804a1c89b
RonenSherman <roshersnowy@gmail.com>
2024-12-10 08:51:03 -0800

    game of life Mouse Input

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,58 +1,25 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
-import java.nio.file.Files;
 
-import static java.nio.file.Files.readString;
 
-public class GameOfLife { // Ronen Sherman - Conway's Game of Life using  java swing
-   static boolean DoGame = true;
-
+public class GameOfLife {// Ronen Sherman - Conway's Game of Life using  java swing
+    // Had to use global variables to pass them to eventListeners without too much difficulty
+    public static boolean DoGame = false;
+    public static int generation = 0;
+    public static int liveCells = 0;
+    public static int[][] Grid = new int[500][500];
+    public static DrawingPanel panel = new DrawingPanel(3000, 2000);
+    public static Graphics g = panel.getGraphics();
     public static void main(String[] args) throws InterruptedException {
-        int w = 500;
-        int h = 500;
-        int[][] Grid = new int[w][h];
-
-
-        StartGameOfLife(w, h, Grid); //starts the game
-        //makeGui();
+        StartGameOfLife(Grid); //starts the game
     }
 
-    static void makeGui() // creates gui
-    {
-        JFrame frame = new JFrame("Conway's Game of Life");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 250);
-        frame.setLayout(new FlowLayout());
-
-
-        JButton button1 = new JButton();
-        button1.setText("New game of life");
-        button1.setPreferredSize(new Dimension(300, 100));
-        button1.addActionListener(new StartsGame());
-        frame.add(button1);
-        frame.setVisible(true);
-
-    }
-
-    static void StartGameOfLife(int x, int y, int[][] Grid) throws InterruptedException {
-        int generation = 0;
-
-        int liveCells = 0;
-        JFrame frame = new JFrame("Conway's Game of Life - generation: " + generation + " Live cells: " + liveCells);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(6000, 4000);
-        frame.setLayout(new FlowLayout());
-
-        DrawingPanel panel = new DrawingPanel(6000, 4000);
-        Graphics g = panel.getGraphics();
+    static void StartGameOfLife(int[][] Grid) throws InterruptedException {
         g.setColor(Color.BLACK);
         panel.setVisible(true);
         panel.setGridLines(false);
-        panel.addKeyListener(new StartsGame());
-        System.out.println(x + " " + y);
-
+        panel.addKeyListener(new TakeInputs());
+        panel.addMouseListener(new TakeInputs());
 
         Grid[110][110] = 1;
         Grid[110][109] = 1;
@@ -60,22 +27,17 @@ public class GameOfLife { // Ronen Sherman - Conway's Game of Life using  java s
         Grid[110][111] = 1;
         Grid[111][111] = 1;
 
-    Boolean Done = false;//
+
         int size = 12;
-    while(!Done) {
-            while (DoGame) {
+        PrintVis(Grid, size, g);// --> initial print to start the game.
+        final boolean isEricPaceLate = true;
+        while (isEricPaceLate) {
+            Thread.sleep(50);
+            if (DoGame) {
+                liveCells = 0;// helps keep track of living cells during the game, outputted during pause using system.out.PrintLn
                 generation++;
-                for (int i = 0; i < Grid.length; i++) {// iterates over the matrix and prints it
-                    for (int j = 0; j < Grid.length; j++) {
-                        if (Grid[i][j] == 0) {
-                            g.setColor(Color.BLACK);// dead cells
-                            g.fillRect(i * size, j * size, size, size);
-                        } else {
-                            g.setColor(Color.WHITE);// living cells
-                            g.fillRect(i * size, j * size, size, size);
-                        }
-                    }
-                }// describes logic below.
+                PrintVis(Grid, size, g); // main printing call that is called every time to reprint the game.
+                // describes logic below.
     /* Creates a function that will check if the cells neighbor is legal to check -
         Ex. if the cell is at 100,0 then checking [i][j-1] would give an index out of bounds error, but if we check legality it would just return 0.
         use two nested for loops to iterate over this array and check each cell int[1][1] ect… -1 represents previous cell,
@@ -98,25 +60,33 @@ public class GameOfLife { // Ronen Sherman - Conway's Game of Life using  java s
                                         + Value(i + 1, j, Grid, Grid.length);
                         if ((sum == 2 || sum == 3) && Grid[i][j] == 1) { // checks sum to see how the [i][j] cell will change
                             OutputM[i][j] = 1;
-                            liveCells++;
                         } else if (sum == 3 && Grid[i][j] == 0) {
                             OutputM[i][j] = 1;
-                            liveCells++;
                         } else {
                             OutputM[i][j] = 0;
                         }
                     }
                 }
+
                 Grid = OutputM; // sets grid to output
             }
         }
     }
-
-    public void onMouseClick(DrawingPanel.DPMouseEventHandler e, int[][] Grid) {
-
-
+    public static void PrintVis(int[][] Grid, int size, Graphics g)// takes the grid, the size, and the graphics.
+    {
+        for (int i = 0; i < Grid.length; i++) {// iterates over the matrix and prints it
+            for (int j = 0; j < Grid.length; j++) {
+                if (Grid[i][j] == 0) {
+                    g.setColor(Color.BLACK);// dead cells
+                    g.fillRect(i * size, j * size, size, size);
+                } else {
+                    liveCells++;
+                    g.setColor(Color.WHITE);// living cells
+                    g.fillRect(i * size, j * size, size, size);
+                }
+            }
+        }
     }
-
 
     public static int Value(int x, int y, int[][] Grid, int w) {
         if (x < 0 || x >= w - 1 || y >= w - 1 || y < 0) { // checks the legality of the cell, if legal, returns cells value,
@@ -126,26 +96,48 @@ public class GameOfLife { // Ronen Sherman - Conway's Game of Life using  java s
         }
     }
 
-
-    public static class StartsGame implements ActionListener, KeyListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //  StartGameOfLife();
-        }
-
+    public static class TakeInputs implements MouseListener, KeyListener {
         @Override
         public void keyTyped(KeyEvent e) {
-        }
 
+        }
         @Override
         public void keyPressed(KeyEvent e) {
+
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_SPACE) { // if the key pressed is space, it flips DoGame, and prints some numbers.
+               DoGame = !DoGame;
+                if(!DoGame)
+                    System.out.println("Conway's Game of Life - generation: " + generation + " Live cells: " + liveCells);
+            }
         }
 
         @Override
-        public void keyReleased(KeyEvent e) {
-            if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-               DoGame = !DoGame;
-            }
+        public void mouseClicked(MouseEvent e) { // this function is a work in progress. will most likely not work, unfortunately.
+            int x = e.getX()/12;
+            int y = e.getY()/12;
+            if(Grid[x][y] == 0) Grid[x][y] = 1;
+            else Grid[x][y] = 0;
+            PrintVis(Grid,12, g);
+
+        }
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+        @Override
+        public void mouseExited(MouseEvent e) {
+
         }
     }
 }
