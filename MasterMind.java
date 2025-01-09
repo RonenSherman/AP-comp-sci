7c4db437c57c9ac5105597fbd12171b63bd7e537
Ronen Sherman <166041889+RonenSherman@users.noreply.github.com>
2025-01-09 11:41:14 -0800

    minor fixes + comments

--- MasterMind.java
+++ MasterMind.java
@@ -1,77 +1,123 @@
-import java.util.ArrayList;
+import java.awt.*;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
+
+public static void main() {
+    DrawingPanel panel = new DrawingPanel(1000, 1000);
+    Graphics g = panel.getGraphics();
+    panel.setVisible(true);
+    GenerateMaze(g);
+}
 
 
+public static void GenerateMaze(Graphics g) {//
+    // dimensions of generated maze
+    int rows = 60, columns = 100;
 
-public class MasterMind {
+    // create grid of walls
+    // W = wall, P = path, S = starting point
+    StringBuilder s = new StringBuilder(columns);
+    s.append("W".repeat(columns));
+    char[][] maze = new char[rows][columns];
+    for (int x = 0; x < rows; x++) maze[x] = s.toString().toCharArray();
 
-    public static class GameData {
-        public String input = "";
-        public String Code = String.valueOf(ThreadLocalRandom.current().nextInt(1000, 10000 + 1));
-        public boolean[] blackPegLocation = {false,false,false,false};
-        public boolean[] WhitePegLocation = {false,false,false,false};
-       public char[] values = {'1','2','3','4'};
-       public char[] WhitePegValues = {'1','2','3','4'} ;
-        public int BlackPegs = 0;
-        public int guesses = 0;
-        public int WhitePegs = 0;
-    }
+    // select random point and make it start node
+    Point starting = new Point((int) (Math.random() * rows), (int) (Math.random() * columns), null);
+    maze[starting.x][starting.y] = 'S';
+    ComputeFrontierCells(maze, starting, g);
+}
 
-    public static void main(String[] args) {
-        GameData g = new GameData();
-            while(!Objects.equals(g.input, g.Code))
-            {
-                GetGuess(g);
+public static void ComputeFrontierCells(char[][] maze, Point starting, Graphics g) {
+    List<Point> FrontierCells = new ArrayList<>();
+    // Add initial frontier cells
+    for (int x = -1; x <= 1; x++) {
+        for (int y = -1; y <= 1; y++) {
+            if ((x == 0 && y == 0) || (x != 0 && y != 0)) continue;
+            int nx = starting.x + x;
+            int ny = starting.y + y;
+            if (isInBounds(maze, nx, ny) && maze[nx][ny] == 'W') {
+                FrontierCells.add(new Point(nx, ny, starting));
             }
-    }
-
-   static void GetGuess(GameData g) {
-        g.guesses++;
-        if(g.BlackPegs == 0)
-        {
-            g.input = String.valueOf(ThreadLocalRandom.current().nextInt(1000, 10000 + 1));
-            ScoringFunction(g);
-
-        } else {
-            g.values = g.input.toCharArray();
-            for (int i = 0; i < 4; i++) {
-                if (!g.blackPegLocation[i]) {
-                    char newguess = (char)ThreadLocalRandom.current().nextInt('0', '9' + 1);
-                    g.values[i] = newguess;
-                }
-            }
-            g.input = String.valueOf((g.values));
-            ScoringFunction(g);
         }
     }
 
-    static int[] ScoringFunction(GameData g) {
-        g.BlackPegs = 0;
-        g.WhitePegs = 0;
-        List<Integer> list = new ArrayList<>(); // code for - 100% success - it finally works
-        for (int i = 0; i < 4; i++) {
-            if (g.input.charAt(i) == g.Code.charAt(i)) {
-                g.BlackPegs++;
-                g.values[i] = g.Code.charAt(i);
-                g.blackPegLocation[i] = true;
-            }
-            for (int j = 0; j < 4; j++) {
-                if ((g.input.charAt(i) == g.Code.charAt(j)) && !list.contains(j) && g.Code.charAt(j) != g.input.charAt(j) && g.Code.charAt(i) != g.input.charAt(i)) {
-                    list.add(j);
-                    g.WhitePegLocation[j] = true;
-                    g.WhitePegValues[j] = g.input.charAt(j);
-                    g.WhitePegs++;
-                    break;
+    Point last = null;
+
+    // Process the frontier cells
+    while (!FrontierCells.isEmpty()) {
+        Point current = FrontierCells.remove((int) (Math.random() * FrontierCells.size()));
+        Point opposite = current.opposite();
+
+        if (isInBounds(maze, opposite.x, opposite.y) && maze[current.x][current.y] == 'W' && maze[opposite.x][opposite.y] == 'W') {
+            maze[current.x][current.y] = 'P';
+            maze[opposite.x][opposite.y] = 'P';
+            last = opposite;
+
+            // Add new frontier cells
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    if ((x == 0 && y == 0) || (x != 0 && y != 0)) continue;
+                    int nx = opposite.x + x;
+                    int ny = opposite.y + y;
+                    if (isInBounds(maze, nx, ny) && maze[nx][ny] == 'W') {
+                        FrontierCells.add(new Point(nx, ny, opposite));
+                    }
                 }
             }
         }
-        // Return the hit counts in the order required!
-        System.out.println("Code is: " + g.Code + " input is: " + g.input);
-        System.out.println("Guesses is: " + g.guesses);
-        System.out.println(g.BlackPegs);
-        System.out.println(g.WhitePegs);
-        return new int[]{g.BlackPegs, g.WhitePegs};
+
+        // Print the maze at each step
+        PrintCells(g, maze);
+    }
+
+    // Set the endpoint
+    if (last != null) {
+        maze[last.x][last.y] = 'E';
+        PrintCells(g, maze);
+    }
+
+}
+
+
+// Utility function to check if a point is within bounds
+private static boolean isInBounds(char[][] maze, int x, int y) {
+    return x >= 0 && x < maze.length && y >= 0 && y < maze[0].length;
+}
+
+
+public static void PrintCells(Graphics g, char[][] maze) { // iterates over the maze and prints it, same as in game of life
+    for (int i = 0; i < maze.length; i++) {
+        for (int j = 0; j < maze[i].length; j++) {
+            if (maze[i][j] == 'W') {
+                g.setColor(Color.BLACK); // Walls
+            } else if (maze[i][j] == 'S') {
+                g.setColor(Color.GREEN); // Start
+            } else if (maze[i][j] == 'E') {
+                g.setColor(Color.RED); // Endpoint
+            } else {
+                g.setColor(Color.WHITE); // Path
+            }
+            g.fillRect(j * 10, i * 10, 10, 10); // Corrected to (j, i)
+        }
+    }
+}
+
+
+public static class Point { // original point class Eric's is a copy
+    Integer x, y;// x and y of each point, need to use Integer instead of int because CompareTo function requires it.
+    Point parent;
+
+    public Point(int r, int c, Point p) {
+        parent = p;
+        this.x = r;
+        this.y = c;
+    }
+
+    // compute opposite node given that it is in the other direction from the parent
+    public Point opposite() {// used to tunnel and build the maze
+        if (this.x.compareTo(parent.x) != 0)
+            return new Point(this.x + this.x.compareTo(parent.x), this.y, this);
+        if (this.y.compareTo(parent.y) != 0)
+            return new Point(this.x, this.y + this.y.compareTo(parent.y), this);
+        return null;
     }
 }
