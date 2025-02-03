0939cc4a1f32753ace5d060752cceaae49a358d7
Ronen Sherman <166041889+RonenSherman@users.noreply.github.com>
2025-02-03 09:33:31 -0800

    Update RecursionGoalSheets.java

--- RecursionGoalSheets.java
+++ RecursionGoalSheets.java
@@ -1,7 +1,7 @@
 public class RecursionGoalSheets {
 
     public static void main(String[] args) {
-        System.out.println(WritenNums(4));
+        System.out.println(writeSequence(10));
 
     }
 
@@ -9,24 +9,38 @@ public class RecursionGoalSheets {
     {
         if(n == 0)
         {
-         return "*";
+            return "*";
         }else
         {
-             String output = StarString(n-1) ;
+            String output = StarString(n-1) ;
             return output + output;
         }
     }
 
-        public static String WritenNums(int n)
+    public static String WritenNums(int n)
+    {
+        if(n == 0)
         {
-            if(n == 0)
-            {
-                return "1";
-            }else
-            {
-                String output = WritenNums(n-1);
-                return output + "," +  (output+1);
+            return "1";
+        }else
+        {
+            String output = (WritenNums(n-1));
 
+            return  output + "," + (n+1);
+
+        }
+    }
+            public static String writeSequence(int n) {
+            if (n < 1) {
+                throw new IllegalArgumentException("n must be at least 1");
+            }
+            if (n == 1) {
+                return "1";
+            } else if (n == 2) {
+                return "1 1";
+            } else {
+                int num = (n + 1) / 2; // Determine the outermost number
+                return num + " " + writeSequence(n - 2) + " " + num;
             }
         }
 
