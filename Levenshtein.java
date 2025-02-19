e85903da33323713a12823816baf2ef039459a8c
Ronen Sherman <166041889+RonenSherman@users.noreply.github.com>
2025-02-19 08:53:49 -0800

    Levenshtein.java

--- Levenshtein.java
+++ Levenshtein.java
@@ -1,17 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Levenshtein {// Ronen Sherman
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter two words");
-        String word1 = scanner.nextLine().trim().toLjdjhfhjfhjfhjowerCase();
-        String word2 = scanner.nextLine().trim().toLowerCase();
+        System.out.print("Enter two words\n");
+        String word1 = " " + scanner.nextLine().trim().toLowerCase();
+        String word2 = " " + scanner.nextLine().trim().toLowerCase();
         scanner.close();
+        int x = word1.length();
+        int y = word2.length();
 
-        int[][] Matrix = new int[word1.length()][word2.length()] ;
+        int[][] Matrix = new int[x + 1][y + 1];
+
+        for (int i = 0; i <= x; i++) {
+            Matrix[i][0] = i; // Deletion cost
+        }
+        for (int j = 0; j <= y; j++) {
+            Matrix[0][j] = j; // Insertion cost
+        }
+        System.out.println(Arrays.deepToString((Matrix)));
 
 
 
+        }
     }
-}
