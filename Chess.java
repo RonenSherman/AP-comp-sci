35900d6bf645dfeda2fb393bec49e3a1ee2d6112
Ronen Sherman <166041889+RonenSherman@users.noreply.github.com>
2025-03-11 08:48:59 -0700

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -4,73 +4,54 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-/*public  interface Piece{
 
-    public static int xpos = 0;
-    int ypos = 0;
-
-    String name = "";
-    char piece = ' ';
-}
-
-public static class Pawn implements Piece
-{
-    xpos = 6;
-} */
 public class Chess { // Ronen Sherman - chess and chess bot
 
+    public void main() {
 
-    public static void main()
-    {
-        Board.g.setColor(Color.BLACK);
         Board.panel.setVisible(true);
-        //Board.panel.setGridLines(true);
         Board.panel.addKeyListener(new TakeInputs());
         Board.panel.addMouseListener(new TakeInputs());
-        for (int i = 0; i < Board.Board.length; i++) {// iterates over the board and prints it
-            for (int j = 0; j < Board.Board.length; j++) {
-                if ((Board.Board[i][j] % 2) == 0) {
-                    Board.g.setColor(Color.BLACK);//
-                    Board.g.fillRect(i * 100, j * 75, 100, 75);
-                } else {
-                    Board.g.setColor(Color.WHITE);//
-                    Board.g.fillRect(i * 100, j * 75, 100, 75);
-                }
-            }
-        }
+
+
+        Board.PrintBoard(75);
     }
 
 
+    public static class Board {
 
 
-
-    public static class Board
-    {
-        public static int[][] Board = new int[8][8];
-        public static DrawingPanel panel = new DrawingPanel(850, 750);
+        public static int[][] board = new int[8][8];
+        public static DrawingPanel panel = new DrawingPanel(600, 650);
         public static Graphics g = panel.getGraphics();
 
-        public static void PrintBoard(int[][] Grid, int size, Graphics g)// takes the grid, the size, and the graphics.
+        public Board() {
+            for (int row = 0; row < 8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    board[row][col] = (row + col) % 2;  // Alternates between 0 and 1
+                }
+            }
+
+        }
+
+        public static void PrintBoard( int size)
         {
-            for (int i = 0; i < Grid.length; i++) {// iterates over the matrix and prints it
-                for (int j = 0; j < Grid.length; j++) {
-                    if (Grid[i][j] == 0) {
-                        g.setColor(Color.BLACK);//
+            for (int i = 0; i < board.length; i++) {// iterates over the board and prints it
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] == 0) {
+                        g.setColor(Color.WHITE);//
                         g.fillRect(i * size, j * size, size, size);
                     } else {
-                        g.setColor(Color.WHITE);//
+                        g.setColor(Color.BLACK);//
                         g.fillRect(i * size, j * size, size, size);
                     }
                 }
             }
         }
     }
-    
 
 
-
-
-    public static class TakeInputs extends Chess.Board implements MouseListener, KeyListener {
+    class TakeInputs extends Chess.Board implements MouseListener, KeyListener {
         @Override
         public void keyTyped(KeyEvent e) {
 
@@ -97,9 +78,9 @@ public class Chess { // Ronen Sherman - chess and chess bot
         public void mouseClicked(MouseEvent e) { // this function is a work in progress. will most likely not work, unfortunately.
             int x = e.getX() / 10;
             int y = e.getY() / 10;
-            if (Board[x][y] == 0) Board[x][y] = 1;
-            else Board[x][y] = 0;
-       //     PrintVis(Grid, 10, g);
+            if (board[x][y] == 0) board[x][y] = 1;
+            else board[x][y] = 0;
+            //     PrintVis(Grid, 10, g);
 
         }
 
